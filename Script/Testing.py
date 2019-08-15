c2011de8bd52bcc8efc7b61d3037842b65a92b16
nicdeca <nico.decarli@live.it>
2019-08-15 12:26:34 +0200

    Update Testing.py

--- Script/Testing.py
+++ Script/Testing.py
@@ -1,29 +1,20 @@
 from Script.Model.whole_model import whole_model
 from Script.Manage_dataset.Data_generator import data_gen
+from Script import Predict
 from keras.layers import Input
 import numpy as np
-
-
-##### DA MODIFICARE:  l'istanza del model deve eseere coerente con quella di training
-input_tensor = Input(shape=(None, None, 3))
-
-model = whole_model(input_tensor)
 
 BATCH_SIZE = 4      # Scelto da me abbastanza random, ragionevole
 NO_OF_TESTING_IMAGES = len(os.listdir(test_frame_path))
 
-test_frame_path = 'https://raw.githubusercontent.com/AlbertoGhiotto/group_project/master/Dataset/test_frames'
-test_gen = data_gen(test_frame_path, batch_size = BATCH_SIZE)
+def test(model, stride):
 
-predictions = model.predict_generator( test_gen, steps=(NO_OF_TESTING_IMAGES//BATCH_SIZE) )
+    test_frame_path = 'https://raw.githubusercontent.com/AlbertoGhiotto/group_project/master/Dataset/test_frames'
+    test_gen = data_gen(test_frame_path, batch_size = BATCH_SIZE)
 
-num_joints = predictions.shape[2]
-pose = []
-for joint_idx in range(num_joints):
-    maxloc = np.unravel_index(np.argmax(predictions[:, :, joint_idx]), predictions[:, :, joint_idx].shape)
-    pos_f8 = (np.array(maxloc).astype('float') * stride + 0.5 * stride)
-    pose.append(np.hstack((pos_f8[::-1], [predictions[maxloc][joint_idx]])))
-pose = np.array(pose)
+    predictions = model.predict_generator( test_gen, steps=(NO_OF_TESTING_IMAGES//BATCH_SIZE) )
+
+    pose = Predict.argmax_predict(predictions, stride)
 
 ##### DA RICONTROLLARE
 
