72b730500c3d12b9c732df25fff44f706f2d987b
nicdeca <nico.decarli@live.it>
2019-08-20 11:43:48 +0200

    Update prediction.py

--- Script/prediction.py
+++ Script/prediction.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 def argmax_predict(predictions, stride):
     
@@ -9,3 +10,12 @@ def argmax_predict(predictions, stride):
         pos_f8 = (np.array(maxloc).astype('float') * stride + 0.5 * stride)
         pose.append(np.hstack((pos_f8[::-1], [predictions[maxloc][joint_idx]])))
     return np.array(pose)
+
+
+def visualize(image, pose):
+  for joint in range(pose.shape[0]):
+      cx = pose[joint][0]
+      cy = pose[joint][1]  
+  
+  cv2.imshow('pose', image)
+  cv2.circle(image,(int(cx),int(cy)),10,(255,255,255),-11)
