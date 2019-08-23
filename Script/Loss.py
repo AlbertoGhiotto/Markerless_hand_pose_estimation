531aa9d578ad36774b9be6952389a957885139a7
nicdeca <nico.decarli@live.it>
2019-08-23 17:59:03 +0200

    Create Loss.py

---
+++ Script/Loss.py
@@ -0,0 +1,14 @@
+def weighted_cross_entropy(beta):
+  def convert_to_logits(y_pred):
+      # see https://github.com/tensorflow/tensorflow/blob/r1.10/tensorflow/python/keras/backend.py#L3525
+      y_pred = tf.clip_by_value(y_pred, tf.keras.backend.epsilon(), 1 - tf.keras.backend.epsilon())
+
+      return tf.log(y_pred / (1 - y_pred))
+
+  def loss(y_true, y_pred):
+    y_pred = convert_to_logits(y_pred)
+    loss = tf.nn.weighted_cross_entropy_with_logits(logits=y_pred, targets=y_true, pos_weight=beta)
+
+    return tf.reduce_mean(loss)
+
+  return loss
