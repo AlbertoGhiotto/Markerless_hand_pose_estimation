6967639b723b93a6713fd1b82b80a4ae03b69c9d
EnricoCasagrande <erri.casagrande@gmail.com>
2019-08-23 18:12:37 +0200

    Added "weighted_cross_entropy"
    Switched to "weighted_cross_entropy" as custom loss function and added functionalities to be able to resume the training

--- Script/Train.py
+++ Script/Train.py
@@ -1,5 +1,6 @@
 from Script.Model.whole_model import whole_model
 from Script.Manage_dataset.Data_generator import data_gen
+from Script.Loss import loss
 from keras.layers import Input
 from keras.optimizers import Adam
 from keras.model import load_model
@@ -21,19 +22,21 @@ def train(model):
   val_gen = data_gen(val_frame_path, batch_size = BATCH_SIZE)
 
 
-  model.compile( optimizer=Adam(lr=1E-5), loss='categorical_crossentropy', metrics=['accuracy'] )#, loss_weights=[1., 0.2])
+  model.compile( optimizer=Adam(lr=2E-4), loss=weighted_cross_entropy(0.8), metrics=['accuracy'] )
   model.fit_generator( train_gen, epochs=NO_OF_EPOCHS, 
                             steps_per_epoch = (NO_OF_TRAINING_IMAGES//BATCH_SIZE),
                             validation_data=val_gen, 
                             validation_steps=(NO_OF_VAL_IMAGES//BATCH_SIZE) )
 
-  model.save('Model/Model.h5')
-  return model
+  model.save("drive/My Drive/Colab Notebooks/Model/Model.h5")
+  print("Saved model to drive")
 
-
-if __name__ == "__main__":
+try:
+  model = load_model("drive/My Drive/Colab Notebooks/Model/Model.h5", custom_objects={'loss': weighted_cross_entropy(0.8)})
+  print("Model loaded from Drive")
+except IOError:
   try:
-    model = load_model('Model/Model.h5')
+    model.load_weights('./Checkpoints/my_checkpoint')
   except IOError:
     model = whole_model()
-  train(model)
+train(model)
