99c88d04f670a30c57b2b3c83ddcfde18a295b66
nicdeca <nico.decarli@live.it>
2019-08-24 12:29:40 +0200

    Update predict_videos.py

--- Script/predict_videos.py
+++ Script/predict_videos.py
@@ -38,5 +38,5 @@ def video_prediction(video, model, stride, show_result = True):
 
 if __name__ == "__main__":
     video_path = 'group_project/test.mp4'
-    model = load_model('/content/drive/My Drive/Colab Notebooks/Model/Model.h5', custom_objects={'loss': weighted_cross_entropy(0.8)})
+    model = load_model('Model/Model.h5', custom_objects={'loss': weighted_cross_entropy(0.8)})
     video_prediction(video_path, model, STRIDE)
